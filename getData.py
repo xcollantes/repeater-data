547b7993906808dd6c15108f9d1b5f2944ddb5cd
Xavier Collantes <collantes.xavier@gmail.com>
2022-02-03 19:42:23 -0800

    Add request functions

--- getData.py
+++ getData.py
@@ -3,28 +3,80 @@
 import json
 import os
 import requests
+from bs4 import BeautifulSoup as Soup
 from absl import app
 from absl import logging
 from absl import flags
 
-logging.set_verbosity(logging.DEBUG)
 FLAGS = flags.FLAGS
 flags.DEFINE_string("data_path", None, "Output location of data file.")
+flags.DEFINE_string("webdriver", None, "Path to webdriver.")
+flags.DEFINE_string("browser", None, "Path to web browser.")
+flags.mark_flag_as_required("data_path")
+flags.mark_flag_as_required("webdriver")
+flags.mark_flag_as_required("browser")
+
+logging.set_verbosity(logging.DEBUG)
 
 
-def scrape(argv) -> None:
-    logging.debug("Strarting scrape...")
-    COUNTRY_CODE: str = "US"
-    STATE_ID: int = 53
-    BASE_URL: str = ("https://www.repeaterbook.com/repeaters/index.php"
-                     + f"?country_code={COUNTRY_CODE}&state_id={STATE_ID}"
-                     + f"&loc=%&call=%&use=%")
+def getStateName(responseText: str) -> str:
+    """Parse state name in title field.
 
-    response = requests.get(BASE_URL)
-    logging.info("RES: %s: %s", response.status_code, response.text)
-    with open(FLAGS.data_path, "w") as dataFile:
-        dataFile.write(response.text)
+    Args:
+        responseText: response.text object from requests.
+
+    Returns: State string name.
+    """
+    soup = Soup(responseText, "html.parser")
+    logging.debug("Ingesting soup: %s", soup.prettify())
+    return soup.title.get_text()
+
+
+def writeToFile(content: str, fileout: str) -> None:
+    """File output handler.
+
+    Args:
+        content: Data to write to file.
+        fileout: Path for file location.
+    """
+    soup = Soup(content, "html.parser")
+    with open(fileout, "w") as dataFile:
+        dataFile.write(soup.prettify())
+
+
+def sendRequest(countryCode: str, stateId: int,
+                bandId: int) -> requests.Response:
+    """Prepare url and send ping to RepeaterBook.
+
+    Args:
+        courntryCode: Two letter country code.
+        stateId: Repeater Book's special ID for each state.
+        bandId: Repeater Books's special ID for bands.
+
+    Returns: Response object.
+    """
+    res: requests.response = None
+    try:
+        url: str = ("https://www.repeaterbook.com/repeaters/Display_SS.php"
+                    + f"?country_code={countryCode.upper()}&state_id={stateId}"
+                    + f"&band={bandId}&loc=%&call=%&use=%")
+        res = requests.get(url)
+    except Exception as e:
+        logging.warning("Could not ping RepeaterBook: %s", e)
+    return res
+
+
+def main(argv) -> None:
+    logging.debug("Starting scrape...")
+
+    # COUNTRY_CODE: str = "US"
+    # STATE_ID: int = 53
+    # BAND_ID: int = 14  # 2 meter
+
+    response = sendRequest(countryCode="US", stateId=53, bandId=14)
+    writeToFile(response.text, FLAGS.data_path)
+    logging.info(getStateName(response.text))
 
 
 if __name__ == "__main__":
-    app.run(scrape)
+    app.run(main)
